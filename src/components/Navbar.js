6175c62f9a30daeaf6cd6aea89a70b49d20b1e16
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-06-08 10:52:36 +0200

    Create navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,20 @@
+import {links} from '../utils/data';
+import {Link} from 'react-router-dom';
+
 const Navbar = () => {
-return <>navbar</>
+  return (
+    <nav className="navbar">
+      <ul>
+        {links.map((link) => {
+          return <li key={link.index}>
+            <Link to={link.url}>{link.text}</Link>
+          </li>
+        })}
+      </ul>
+    </nav>
+  )
+  
+
 }
 
 export default Navbar;
