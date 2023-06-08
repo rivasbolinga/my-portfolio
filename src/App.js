e324f8eae93b36398921248b762cce0869d56773
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-06-08 10:40:53 +0200

    Create Navbar and Sidebar files

--- src/App.js
+++ src/App.js
@@ -1,44 +1,34 @@
 
 import './App.css';
 import { 
-  createBrowserRouter, 
-  RouterProvider, 
-  createRoutesFromElements, 
-  Route, 
-  Link, } from 'react-router-dom';
+  createBrowserRouter,
+  Routes,
+  Route,
+ } from 'react-router-dom';
 import {
   HomePage,
   AboutPage,
   ContactPage,
   WorksPage,
+  ErrorPage
 } from './pages/index'
 
 
-function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<Root />}>
-        <Route index element={<HomePage />} />
+const App = () => {
+  const BrowserRouter = createBrowserRouter();
+
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/works" element={<WorksPage />} />
-      </Route>
-    )
-  )
-  return (
-    <div className="App">
-      <RouterProvider router={router} />
-    </div>
-  );
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>)
 }
-const Root = () => {
-return (
-  <>
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/contact">Contact</Link>
-    </div>
-  </>
-)
-}
+
 export default App;
---
+++ src/utils/data.js
@@ -0,0 +1,22 @@
+export const links = [
+  {
+    id: 1,
+    text: 'home',
+    url: '/',
+  },
+  {
+    id: 2,
+    text: 'about',
+    url: '/about',
+  },
+  {
+    id: 3,
+    text: 'works',
+    url: '/works',
+  },
+  {
+    id: 4,
+    text: 'contact',
+    url: '/contact',
+  },
+]
