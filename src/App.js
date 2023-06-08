5636e6a17ae2cdf381cfe8c0569b4ae8bfc016e5
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-06-08 08:07:40 +0200

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
 import './App.css';
 import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route, Link } from 'react-router-dom';
+import {
+  HomePage,
+  AboutPage,
+  ContactPage,
+  WorksPage
+} from './pages/index'
+
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<Root />}></Route>
+      <Route path="/" element={<Root />}>
+        <Route index element={<HomePage />} />
+        <Route element={<ContactPage />} />
+        <Route element={<AboutPage />} />
+        <Route element={<WorksPage />} />
+      </Route>
     )
   )
   return (
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import HomePage from "./HomePage";
+import AboutPage from "./AboutPage";
+import ContactPage from "./ContactPage";
+import WorksPage from "./WorksPage";
+
+export {
+  HomePage,
+  AboutPage,
+  ContactPage,
+  WorksPage
+}
