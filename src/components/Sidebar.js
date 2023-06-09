4750ef7ca2e7e6a7c6ae7e60d3fb91e2dd3d0368
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-06-09 12:05:51 +0200

    Finish navigation styles

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -10,28 +10,28 @@ const Sidebar = () => {
   const { isSidebarOpen, closeSidebar } = useNavigationContext()
 
   return (
-    <aside className={`${isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}`}>
-      <div className="sidebar-header">
-        <img src={logo} className="logo-sidebar" alt="coding addict" />
-        <button className="close-sidebar" onClick={closeSidebar}>
-          <TfiClose />
-        </button>
-      </div>
-      <ul className="sidebar-links">
-        {links.map(({ id, text, url }) => {
-          return (
+    <aside className={`sidebar ${isSidebarOpen ? 'show-sidebar' : ''}`}>
+      <div className="side-menu__overlay"></div>
+      <div className="sidebar-content">
+        <div className="sidebar-header">
+          <img src={logo} className="logo-sidebar" alt="coding addict" />
+          <button className="close-sidebar" onClick={closeSidebar}>
+            <span>&times;</span>
+          </button>
+        </div>
+        <ul className="sidebar-links">
+          {links.map(({ id, text, url }) => (
             <li key={id}>
               <Link to={url} onClick={closeSidebar}>
                 <span>{text.slice(0, 1)}</span>
                 {text.slice(1)}
               </Link>
             </li>
-          )
-        })}
-      </ul>
+          ))}
+        </ul>
+      </div>
     </aside>
   )
 }
-
 
 export default Sidebar
