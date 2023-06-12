dc3f12c5f269b35683ed90d7ee4bc8b3c6f8d5be
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-06-12 10:30:46 +0200

    STyle Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,18 +1,24 @@
 import '../styles/hero.scss';
+import { AiOutlineArrowRight } from 'react-icons/ai';
 
 const Hero = () => (
   <section className="hero-section">
-    <div className="name-container">
-      <h1>andrea rivas</h1>
-    </div>
-    <div className="text-container">
-      <h2>
-        Font-end developer. Passionate about creating unforgetable experiences
-        {' '}
-      </h2>
-    </div>
-    <div className="image-container">
-      animation1
+    <main>
+      <div className="name-container">
+        <h1>andrea</h1>
+        <h1>rivas</h1>
+      </div>
+      <div className="text-container">
+        <h2>
+          Font-end developer. Passionate about creating unforgettable
+          experiences
+          {' '}
+        </h2>
+      </div>
+    </main>
+    <div className="scroll-right">
+      <p>scroll right</p>
+      <AiOutlineArrowRight />
     </div>
   </section>
 );
